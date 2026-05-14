2e00d9de1e1488058d209b075f858d7a174af3cd
Anzhalika Sokalava <anzhalika_sokalava@epam.com>
2026-05-14 11:11:29 +0200

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -102,6 +103,7 @@ export default function RootLayout({
   </div>
 </footer>
   <Analytics />
+  <GoogleAnalytics gaId="G-Y9EP52NP5T" />
 </body>
     </html>
   );
