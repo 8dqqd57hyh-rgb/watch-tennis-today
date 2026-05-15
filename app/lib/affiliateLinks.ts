6e246b2fd1746aa24a318cfdd04ed28084830c71
Anzhalika Sokalava <anzhalika_sokalava@epam.com>
2026-05-15 13:19:37 +0200

    Add NordVPN affiliate link

--- app/lib/affiliateLinks.ts
+++ app/lib/affiliateLinks.ts
@@ -5,4 +5,6 @@ export const affiliateLinks = {
   skySports: "https://www.skysports.com/tennis",
   bbc: "https://www.bbc.co.uk/sport/tennis",
   canalPlus: "https://www.canalplus.com/",
+  nordvpn:
+    "https://go.nordvpn.net/aff_c?offer_id=15&aff_id=148020&url_id=902",
 };
